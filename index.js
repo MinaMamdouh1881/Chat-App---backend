199846911229248e16d6279a8a401a0bf4a60494
Mina Mamdouh <menamamdouh852456@gmail.com>
2024-08-27 13:28:08 +0300

    make authentication system and send message controller

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectToMongoDB from './DB/connectToMongodb.js';
 import authRoute from './routes/auth.route.js';
+import messageRoute from './routes/message.route.js';
 
 dotenv.config();
 
@@ -11,9 +13,11 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use('/api/auth', authRoute);
+app.use('/api/message', messageRoute);
 
 app.listen(PORT, () => {
   connectToMongoDB();
